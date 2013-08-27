b15d815d49f1ee255c744b09fd93bf1faa374ea1
Iulian Anghel <iulian.anghel@gmail.com>
2013-08-27 18:02:11 +0300

    Added Back button to the header.

---
+++ js/blanky.js
@@ -0,0 +1,21 @@
+/*
+ * throttledresize: special jQuery event that happens at a reduced rate compared to "resize"
+ *
+ * latest version and complete README available on Github:
+ * https://github.com/louisremi/jquery-smartresize
+ *
+ * Copyright 2012 @louis_remi
+ * Licensed under the MIT license.
+ *
+ * This saved you an hour of work?
+ * Send me music http://www.amazon.co.uk/wishlist/HNTU0468LQON
+ */
+(function ($) {
+    $("body").css("padding-top", $(".navbar-fixed-top").height() + 15 );
+    $("body").css("padding-bottom", $(".navbar-fixed-bottom").height() );
+
+    $(window).on("throttledresize", function (event) {
+        $("body").css("padding-top", $(".navbar-fixed-top").height() + 15 );
+        $("body").css("padding-bottom", $(".navbar-fixed-bottom").height());
+    });
+})(jQuery);
